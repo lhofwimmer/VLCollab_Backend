9ceff005e3f946043656dfeeb6f59e6964fe94a9
lhofwimmer <lukas.hofwimmer@gmail.com>
2019-02-06 08:53:21 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vlcollab_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Started server");
+        }
+    }
+}
